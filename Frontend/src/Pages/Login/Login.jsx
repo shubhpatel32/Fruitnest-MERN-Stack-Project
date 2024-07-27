9ca4a3dfe1180b20bc26b5371115f85111e20e93
shubhpatel32 <sdpatel972@gmail.com>
2024-07-27 13:13:43 +0530

    Login Signup UI changes

--- Frontend/src/Pages/Login/Login.jsx
+++ Frontend/src/Pages/Login/Login.jsx
@@ -1,67 +1,62 @@
 import React, { useState } from 'react';
-// import './Login.css';
 import { Link } from 'react-router-dom';
 
 function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [emailError, setEmailError] = useState('');
-    const [passwordError, setPasswordError] = useState('');
+    const [user, setUser] = useState({
+        email: '',
+        password: '',
+    });
 
     const handleLogin = (e) => {
-        e.preventDefault();
+        let name = e.target.name;
+        let value = e.target.value;
 
-        // Reset errors
-        setEmailError('');
-        setPasswordError('');
-
-        // Validation
-        let isValid = true;
-        if (!email) {
-            setEmailError('Email is required.');
-            isValid = false;
-        }
-        if (!password) {
-            setPasswordError('Password is required.');
-            isValid = false;
-        }
-
-        // Submit if valid
-        if (isValid) {
-            // Perform login logic here (e.g., API call)
-            console.log('Logging in with:', email, password);
-        }
+        setUser({
+            ...user,
+            [name]: value
+        })
     };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
 
     return (
         <div>
-            <section className="login bg-[#fff] px-5 py-10  min-h-[100vh] flex items-center justify-center text-2xl w-full">
-                <form onSubmit={handleLogin} className="login-form m-4 p-12 flex flex-col shadow-[0.2rem_0.2rem_0.2rem_0.2rem_#a8a297]">
+            <section className="login px-5 py-10  min-h-[100vh] flex items-center justify-center text-2xl w-full">
+                <form onSubmit={handleSubmit} className="login-form m-4 p-12 flex flex-col shadow-[0.2rem_0.2rem_0.2rem_0.2rem_#a8a297]">
                     <h1 className='text-center text-3xl mb-5 font-semibold'>Login</h1>
+                    <label htmlFor="email">Email</label>
                     <input
                         type="email"
-                        placeholder="Email"
-                        className="box p-2 mb-7 w-full  rounded border border-solid border-[#a8a297]"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
+                        name="email"
+                        id="email"
+                        required
+                        autoComplete='off'
+                        className="box p-2 mb-7 w-full  rounded border border-solid border-[#a8a297] focus:border-[#ff9421]"
+                        value={user.email}
+                        onChange={handleLogin}
                     />
-                    {emailError && <p className="error">{emailError}</p>}
+
+                    <label htmlFor="password">Password</label>
                     <input
                         type="password"
-                        placeholder="Password"
-                        className="box p-2 mb-7 w-full  rounded border border-solid border-[#a8a297]"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
+                        name="password"
+                        id="password"
+                        required
+                        autoComplete='off'
+                        className="box p-2 mb-7 w-full  rounded border border-solid border-[#a8a297] focus:border-[#ff9421]"
+                        value={user.password}
+                        onChange={handleLogin}
                     />
-                    {passwordError && <p className="error">{passwordError}</p>}
-                    <div className="remember flex items-center mb-4">
-                        <input type="checkbox" id="remember-me" className='mr-3 accent-[#ff9421]' />
-                        <label htmlFor="remember-me">Remember me</label>
-                    </div>
+
                     <input type="submit" value="Login" className="btn p-3 rounded text-white cursor-pointer w-full text-2xl" />
+
                     <p className="text-center mt-3 normal-case">
                         Forgot your password? <Link to="#" className="text-[#cf1a1a] hover:underline">Click here</Link>
                     </p>
+
                     <p className="text-center mt-3 normal-case">
                         Don't have an account? <Link to="/signup" className="text-[#cf1a1a] hover:underline">Sign Up</Link>
                     </p>
--- Frontend/src/Pages/SignUp/Signup.jsx
+++ Frontend/src/Pages/SignUp/Signup.jsx
@@ -2,73 +2,78 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Signup() {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [error, setError] = useState('');
+    const [user, setUser] = useState({
+        username: '',
+        email: '',
+        phone: '',
+        password: '',
+    });
 
     const handleSignup = (e) => {
-        e.preventDefault();
+        let name = e.target.name;
+        let value = e.target.value;
 
+        setUser({
+            ...user,
+            [name]: value
+        })
 
-        if (password !== confirmPassword) {
-            setError('Passwords do not match');
-            return;
-        }
-
-        if (!firstName || !lastName || !email || !password) {
-            setError('Please fill in all fields');
-            return;
-        }
-
-
-        console.log('Signup details:', { firstName, lastName, email, password });
-        setError('');
     };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
 
     return (
         <div>
-            <section className="signup bg-[#fff] px-5 py-10  min-h-[100vh] flex items-center justify-center text-2xl w-full">
-                <form onSubmit={handleSignup} className="signup-form  m-4 p-12 flex flex-col shadow-[0.2rem_0.2rem_0.2rem_0.2rem_#a8a297]">
+            <section className="signup px-5 py-10  min-h-[100vh] flex items-center justify-center text-2xl w-full bg-center bg-cover bg-no-repeat" style={{ backgroundImage: `url('/SliderImages/bg16.jpg')` }}>
+                <form onSubmit={handleSubmit} className="signup-form  m-4 p-12 flex flex-col ">
                     <h1 className="text-center text-3xl mb-5 font-semibold">Sign Up</h1>
-                    {error && <p className="error">{error}</p>}
+                    <label htmlFor="username">Username</label>
                     <input
                         type="text"
-                        placeholder="First Name"
-                        className="box p-2 mb-7 w-full  rounded border border-solid border-[#a8a297]"
-                        value={firstName}
-                        onChange={(e) => setFirstName(e.target.value)}
+                        name="username"
+                        id="username"
+                        required
+                        autoComplete='off'
+                        className="box p-2 mb-7 w-full  rounded border border-solid border-black focus:border-[#ff9421] bg-transparent"
+                        value={user.username}
+                        onChange={handleSignup}
                     />
-                    <input
-                        type="text"
-                        placeholder="Last Name"
-                        className="box p-2 mb-7 w-full  rounded border border-solid border-[#a8a297]"
-                        value={lastName}
-                        onChange={(e) => setLastName(e.target.value)}
-                    />
+                    <label htmlFor="email">Email</label>
                     <input
                         type="email"
-                        placeholder="Email"
-                        className="box p-2 mb-7 w-full  rounded border border-solid border-[#a8a297]"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
+                        name="email"
+                        id="email"
+                        required
+                        autoComplete='off'
+                        className="box p-2 mb-7 w-full  rounded border border-solid border-black focus:border-[#ff9421] bg-transparent"
+                        value={user.email}
+                        onChange={handleSignup}
                     />
+                    <label htmlFor="phone">Phone No.</label>
+                    <input
+                        type="Number"
+                        name="phone"
+                        id="phone"
+                        required
+                        autoComplete='off'
+                        className="box p-2 mb-7 w-full  rounded border border-solid border-black focus:border-[#ff9421] bg-transparent"
+                        value={user.phone}
+                        onChange={handleSignup}
+                    />
+                    <label htmlFor="password">Password</label>
                     <input
                         type="password"
-                        placeholder="Password"
-                        className="box p-2 mb-7 w-full  rounded border border-solid border-[#a8a297]"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
+                        name="password"
+                        id="password"
+                        required
+                        autoComplete='off'
+                        className="box p-2 mb-7 w-full  rounded border border-solid border-black focus:border-[#ff9421] bg-transparent"
+                        value={user.password}
+                        onChange={handleSignup}
                     />
-                    <input
-                        type="password"
-                        placeholder="Confirm Password"
-                        className="box p-2 mb-7 w-full  rounded border border-solid border-[#a8a297]"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                    />
+
                     <input type="submit" value="Sign Up" className="btn p-3 rounded text-white cursor-pointer w-full text-2xl" />
                     <p className="text-center mt-3 normal-case">Already have an account? <Link to="/login" className=" text-[#cf1a1a] hover:underline">Login </Link></p>
                 </form>
