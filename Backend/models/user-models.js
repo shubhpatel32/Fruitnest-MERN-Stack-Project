d3f2c8344d307a47d9202cdd84690bbbdf8028de
shubhpatel32 <sdpatel972@gmail.com>
2024-08-12 20:09:42 +0530

    Token expires in a week

--- Backend/models/user-models.js
+++ Backend/models/user-models.js
@@ -41,7 +41,7 @@ userSchema.methods.generateToken = async function () {
       },
       process.env.JWT_SECRET_KEY,
       {
-        expiresIn: "30d",
+        expiresIn: "7d",
       }
     );
   } catch (error) {
