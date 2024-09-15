34ffadeab066a50648d133a8ba8e74f3d1fb6896
shubhpatel32 <sdpatel972@gmail.com>
2024-09-15 12:36:36 +0530

    changed background image in Heading component

--- Frontend/src/Components/Heading.jsx
+++ Frontend/src/Components/Heading.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Heading({ name1, name2 }) {
     return (
         <div>
-            <div className="heading bg-cover bg-center bg-no-repeat text-center pt-48 pb-32" style={{ backgroundImage: `url('/SliderImages/gradientbg.jpg')` }}>
+            <div className="heading bg-cover bg-center bg-no-repeat text-center pt-48 pb-32" style={{ backgroundImage: `url('/SliderImages/heading3.jpg')` }}>
                 <h1 className="text-black text-[4rem] font-semibold">{name1}</h1>
                 <p className="pt-2 text-3xl text-black font-bold">
                     <Link to="/" className="text-black pr-2 hover:text-red-700">Home</Link>
